ea0ad11b52d8475f380bced79b3da3d76b015053
Mamadou Djoulde Barry <mdjouldebarry17@gmail.com>
2025-07-01 12:58:22 +0000

    Traite du deuxieme exos

---
+++ 05_joinStrings/greeting.js
@@ -0,0 +1,3 @@
+const { firstName, lastName, thisYear, birthYear } = require("./joinStrings-example");
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
--- 05_joinStrings/joinStrings-example.js
+++ 05_joinStrings/joinStrings-example.js
@@ -1,9 +1,9 @@
+const { greeting } = require("./greeting");
+
 const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
-
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
 console.log(greeting);
 
